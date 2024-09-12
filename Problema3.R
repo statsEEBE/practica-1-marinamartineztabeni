4c00a03ad2bf49cecbdf2da3cc735d64ab4936eb
marina.martinez.tabeni <marina.martinez.tabeni@auleseebe.upc.edu>
2024-09-12 12:03:16 +0200

    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,13 @@
 #Codigo para problema 3
+
+X<- c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+  #definit vectores con la función c() concatenar
+
+sum(X)
+
+Y<-X[-c(21,2,26)]
+
+Y
+
+sum(exp(X))-sum(exp(Y))
+
